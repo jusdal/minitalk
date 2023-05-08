1187f0246489ee3b417900917ce8e4fa8b8db316
justin daly <justin.m.daly@gmail.com>
2023-05-09 01:16:23 +0700

    add header file

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 00:58:58 by jdaly             #+#    #+#             */
+/*   Updated: 2023/05/09 01:15:10 by jdaly            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
