e8f004bd920d40ae45760a37f90d10fda85549a4
justin daly <justin.m.daly@gmail.com>
2023-05-08 21:13:42 +0700

    move bitcount for better readability

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 01:02:18 by jdaly             #+#    #+#             */
-/*   Updated: 2023/05/08 16:37:44 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/05/08 21:03:53 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,14 +56,13 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd(c[i], fd);
 }
 
-void	sighandler(int signum)
+void	sigusrhandler(int signum)
 {
-	static int	bitcount = 0;
+	static int	bitcount = 1;
 	static int	c = 0;
 
 	if (signum == SIGUSR1)
 		c |= 1;
-	bitcount++;
 	if (bitcount == 8)
 	{
 		ft_putchar_fd(c, 1);
@@ -72,6 +71,7 @@ void	sighandler(int signum)
 	}
 	else
 		c <<= 1;
+	bitcount++;
 }
 
 int	main(void)
@@ -84,8 +84,8 @@ int	main(void)
 	ft_putchar_fd('\n', 1);
 	while (1)
 	{
-		signal(SIGUSR1, sighandler);
-		signal(SIGUSR2, sighandler);
+		signal(SIGUSR1, sigusrhandler);
+		signal(SIGUSR2, sigusrhandler);
 	}
 	return (0);
 }
