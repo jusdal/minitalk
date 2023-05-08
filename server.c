981559dd80376f18a2e446a30be23675c078d17b
justin daly <justin.m.daly@gmail.com>
2023-05-08 16:38:10 +0700

    fix norm errors / delete comments

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 01:02:18 by jdaly             #+#    #+#             */
-/*   Updated: 2023/05/08 16:31:46 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/05/08 16:37:44 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,27 +56,22 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd(c[i], fd);
 }
 
-void sighandler(int signum)
+void	sighandler(int signum)
 {
-	/*if (signum == SIGUSR1 || signum == SIGUSR2)
-		ft_putstr_fd("SIGNAL CAUGHT", 1);*/
-
 	static int	bitcount = 0;
 	static int	c = 0;
 
 	if (signum == SIGUSR1)
-		c |= 1; // sets right bit to 1
-		//ft_putstr_fd("SIGUSR1 RECEIVED", 1);
+		c |= 1;
 	bitcount++;
-	if (bitcount == 8) // checks if 8 bits and prints
+	if (bitcount == 8)
 	{
 		ft_putchar_fd(c, 1);
 		bitcount = 0;
 		c = 0;
 	}
 	else
-		c <<= 1; // shifts bits left by 1, adding a 0 on the right
-		//ft_putstr_fd("ELSE", 1);
+		c <<= 1;
 }
 
 int	main(void)
@@ -87,8 +82,7 @@ int	main(void)
 	ft_putstr_fd("Server PID = ", 1);
 	ft_putnbr_fd(pid, 1);
 	ft_putchar_fd('\n', 1);
-
-	while (1) // listen for signals
+	while (1)
 	{
 		signal(SIGUSR1, sighandler);
 		signal(SIGUSR2, sighandler);
