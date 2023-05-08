4071d27b9888f988b56a010446dbc3b0a1369533
justin daly <justin.m.daly@gmail.com>
2023-05-09 01:27:07 +0700

    fix norm errors

--- minitalk.h
+++ minitalk.h
@@ -6,16 +6,16 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 00:58:58 by jdaly             #+#    #+#             */
-/*   Updated: 2023/05/09 01:15:10 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/05/09 01:25:44 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 #endif
